869480968f954df1f609e897e3ccfba707489497
joechou88 <joechou2002q@gmail.com>
2026-05-12 06:08:57 +0800

    list variables in Stata

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
+import os
+
+# Folder name
+STATA_DIR = "Stata"
+
 # File name
 SDC_INPUT = "Calculated_All_countries_SDC_2015-2024.xlsx"
-STATA_INPUT = "Financial_npv_lease20152024.dta"
-STATA_OUTPUT = "Financial_npv_lease20152024_20260501.dta"
+STATA_INPUT = os.path.join(STATA_DIR, "Financial_npv_lease20152024.dta")
+STATA_OUTPUT = os.path.join(STATA_DIR, "Financial_npv_lease20152024_20260511.dta")
